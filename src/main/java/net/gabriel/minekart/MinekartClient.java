312b1537e3f1b5475d557e6a3afb44a0a85ce69b
Gabriel <gabrieln38@nycstudents.net>
2024-05-24 14:49:22 -0400

    inital commit

---
+++ src/main/java/net/gabriel/minekart/MinekartClient.java
@@ -0,0 +1,10 @@
+package net.gabriel.minekart;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinekartClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
