2949d25797754a32032d67c7822008ecb6b957f5
Gabriel <gabrieln38@nycstudents.net>
2024-05-30 00:50:55 -0400

    B I G I C E C R E A M B U T B E T T E R

--- src/main/java/net/gabriel/minekart/item/custom/VanillaIceCreamItem.java
+++ src/main/java/net/gabriel/minekart/item/custom/VanillaIceCreamItem.java
@@ -1,19 +1,25 @@
-package net.gabriel.minekart.item;
+package net.gabriel.minekart.item.custom;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.gabriel.minekart.Minekart;
+import net.gabriel.minekart.item.ModFoodComponents;
+import net.gabriel.minekart.item.ModItems;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
-public class ModItems {
+public class VanillaIceCreamItem extends Item {
+    public VanillaIceCreamItem(Settings settings) {
+        super(settings);
+    }
+
     public static final Item VANILLA_ICE_CREAM = registerItem("vanilla_ice_cream", new Item(new FabricItemSettings().food(ModFoodComponents.VANILLA_ICE_CREAM)));
 
-    public static void addItemsToFoodAndDrinksItemGroup(FabricItemGroupEntries entries) {
+    private static void addItemsToFoodAndDrinksItemGroup(FabricItemGroupEntries entries) {
         entries.add(VANILLA_ICE_CREAM);
     }
     private static Item registerItem(String name, Item item) {
