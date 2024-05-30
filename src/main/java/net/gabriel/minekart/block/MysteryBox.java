a59226d6294d2c8c04db4ef4e1bcd9782561baea
BT_2N2_12 <@>
2024-05-30 13:56:30 -0400

    mystery_block w/o images

---
+++ src/main/java/net/gabriel/minekart/block/MysteryBox.java
@@ -0,0 +1,30 @@
+package net.gabriel.minekart.block;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.gabriel.minekart.Minekart;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
+
+public class MysteryBox {
+    public static final Block MYSTERY_BLOCK = registerBlock("mystery_block", new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK)));
+
+    private static Block registerBlock(String name, Block block) {
+        registerMysteryBoxItem(name, block);
+        return Registry.register(Registries.BLOCK, new Identifier(Minekart.MOD_ID, name), block);
+    }
+
+    private static Item registerMysteryBoxItem(String name, Block block) {
+        return Registry.register(Registries.ITEM, new Identifier(Minekart.MOD_ID, name), new BlockItem(block, new FabricItemSettings()));
+    }
+
+    public static void registerModBlocks() {
+        Minekart.LOGGER.info("Registering MysteryBox for " + Minekart.MOD_ID);
+    }
+}
