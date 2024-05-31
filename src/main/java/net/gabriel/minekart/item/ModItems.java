fe17d8220be2e7ad51a18af1775b5d17d15f86af
BT_2N2_13 <aali8280@bths.edu>
2024-05-31 13:53:54 -0400

    Have to add some code for the speed bump to work and some texture

--- src/main/java/net/gabriel/minekart/item/ModItems.java
+++ src/main/java/net/gabriel/minekart/item/ModItems.java
@@ -12,9 +12,15 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
     public static final Item VANILLA_ICE_CREAM = registerItem("vanilla_ice_cream", new Item(new FabricItemSettings().food(ModFoodComponents.VANILLA_ICE_CREAM)));
+    public static final Item SPEED_BUMP = registerItem("speed_bump", new Item(new FabricItemSettings()));
+
 
     public static void addItemsToFoodAndDrinksItemGroup(FabricItemGroupEntries entries) {
         entries.add(VANILLA_ICE_CREAM);
+    }
+
+    public static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(SPEED_BUMP);
     }
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(Minekart.MOD_ID, name), item);
@@ -22,6 +28,8 @@ public class ModItems {
 
     public static void registerModItems() {
         Minekart.LOGGER.info("Registering Mod Items for " + Minekart.MOD_ID);
+
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(ModItems::addItemsToFoodAndDrinksItemGroup);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
     }
 }
