e352875106908558e52827ac2e511056a90dab8d
Gabriel <gabrieln38@nycstudents.net>
2024-06-05 12:52:27 -0400

    started making gui, a command, and started fucking around with nbt data

--- src/main/java/net/gabriel/minekart/item/ModItems.java
+++ src/main/java/net/gabriel/minekart/item/ModItems.java
@@ -5,7 +5,6 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.gabriel.minekart.Minekart;
 import net.gabriel.minekart.item.custom.SpeedBumpItem;
-import net.gabriel.minekart.item.custom.VanillaIceCreamItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
