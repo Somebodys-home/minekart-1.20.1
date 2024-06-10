ef82d7289c4f4e923e425bf1c12aa79d925ddabf
Atif Ali_ <aatif1771@gmail.com_>
2024-06-09 20:59:53 -0400

    If you are on a boat, and you have SpeedBumpItem, it will float you to the air no matter where you are (water or ice doesn't matter) Probably have to add a cooldown and horizontal velocity

--- src/main/java/net/gabriel/minekart/item/custom/SpeedBumpItem.java
+++ src/main/java/net/gabriel/minekart/item/custom/SpeedBumpItem.java
@@ -7,6 +7,7 @@ import net.gabriel.minekart.Minekart;
 import net.gabriel.minekart.item.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.vehicle.BoatEntity;
 import net.minecraft.item.Item;
@@ -28,10 +29,6 @@ public class SpeedBumpItem extends Item {
         super(settings);
     }
 
-    public ActionResult useOnBlock(ItemUsageContext context) {
-        return use(context.getWorld(), context.getPlayer(), context.getHand()).getResult();
-    }
-
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
         ItemStack stack = player.getStackInHand(hand);
@@ -39,18 +36,35 @@ public class SpeedBumpItem extends Item {
         if (!world.isClient) {
             if (player.hasVehicle() && player.getVehicle() instanceof BoatEntity) {
                 BoatEntity boat = (BoatEntity) player.getVehicle();
-                boat.setVelocity(boat.getVelocity().add(0, 1, 0));
-                boat.velocityModified = true;
-                player.sendMessage(Text.literal("test"), true);
+                applySpeedBumpEffect(boat);
+                player.sendMessage(Text.literal("You used the Speed Bump while on a boat!"), true);
             } else {
-                player.setVelocity(player.getVelocity().add(0, 1, 0));
-                player.velocityModified = true;
-                player.sendMessage(Text.literal("test"), true); //delete later
+                applySpeedBumpEffect(player);
+                player.sendMessage(Text.literal("You used the Speed Bump!"), true);
             }
         }
 
         return new TypedActionResult<>(ActionResult.SUCCESS, stack);
     }
 
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        if (!world.isClient && entity instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) entity;
+            if (selected && player.hasVehicle() && player.getVehicle() instanceof BoatEntity) {
+                BoatEntity boat = (BoatEntity) player.getVehicle();
+                applySpeedBumpEffect(boat);
+            }
+        }
+    }
 
+    private void applySpeedBumpEffect(BoatEntity boat) {
+        boat.setVelocity(boat.getVelocity().add(0, 1, 0));
+        boat.velocityModified = true;
+    }
+
+    private void applySpeedBumpEffect(PlayerEntity player) {
+        player.setVelocity(player.getVelocity().add(0, 1, 0));
+        player.velocityModified = true;
+    }
 }
